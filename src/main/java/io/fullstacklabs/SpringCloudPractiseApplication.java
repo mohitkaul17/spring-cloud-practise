e75a4c7c96040d9c4ada807a73ab56f52804cadb
kaulmohit17 <mohit@fullstacklabs.ca>
2021-02-18 10:25:27 -0500

    feat(project):clean state

---
+++ src/main/java/io/fullstacklabs/SpringCloudPractiseApplication.java
@@ -0,0 +1,13 @@
+package io.fullstacklabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudPractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudPractiseApplication.class, args);
+    }
+
+}
